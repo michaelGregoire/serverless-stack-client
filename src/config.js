ac74369da760d51f3f510e248a122fa5e40f26d8
Mike Gregoire <mgregoire@nvzion.com>
2021-06-01 16:06:06 -0400

    Wrap up work on client

--- src/config.js
+++ src/config.js
@@ -1,17 +1,19 @@
 const config = {
+  STRIPE_KEY: 'pk_test_CtdVJXEImW0qIAqORmdbAzzM',
+  MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
     REGION: 'us-east-1',
-    BUCKET: 'mg-notes-app-uploads',
+    BUCKET: 'mg-notes-app-uploads-2',
   },
   apiGateway: {
     REGION: 'us-east-1',
-    URL: 'https://lfkfj0z2nc.execute-api.us-east-1.amazonaws.com/prod',
+    URL: 'https://zi128t5kjk.execute-api.us-east-1.amazonaws.com/prod',
   },
   cognito: {
     REGION: 'us-east-1',
-    USER_POOL_ID: 'us-east-1_HEzubOlae',
-    APP_CLIENT_ID: '5hbq82mnpdo4cvcahkcnu4aqb8',
-    IDENTITY_POOL_ID: 'us-east-1:bf4d81e0-559b-4eb1-8b56-2ee4dab60be3',
+    USER_POOL_ID: 'us-east-1_ftk6T3o9s',
+    APP_CLIENT_ID: '3k161t5lvvirdapvtv9i3m9mfi',
+    IDENTITY_POOL_ID: 'us-east-1:f987de8c-cfc2-4b53-9b0d-ce1d1226aed5',
   },
 };
 
